17ad21b05200f85a6b6ad6cd16db83be80bc9958
Charlie-Gauvin <gauvinch9@gmail.com>
2024-12-09 15:34:23 +0100

    Config CORS

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,13 @@ export default [
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
-  "strapi::cors",
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [process.env.URL_PRODUCTION, 'http://localhost:3000'], // Remplacez par les URL de votre front-end
+    },
+  },
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
